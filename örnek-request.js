bdfbd90d1e224976e3014c73ecb51cf9ed1a65bd
Vodkâ <65621796+tunadnz@users.noreply.github.com>
2021-02-17 03:24:57 +0300

    Create örnek-request.js

---
+++ örnek-request.js
@@ -0,0 +1,16 @@
+const { Discord, MessageEmbed } = require("discord.js");
+const axios = require('axios');
+
+exports.run = async (client, message, args) => {
+  
+axios.get(`https://proje-adi.com/api/apiad`)
+.then((response) => {
+
+const embed = new MessageEmbed()
+.setColor('RANDOM')
+.addField(`Veri 1`, response.data.veri1, true)     
+.addField(`Veri 2`, response.data.veri2, true) 
+console.log(response)
+message.channel.send(embed);        
+});
+};
